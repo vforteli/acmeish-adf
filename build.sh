81d973e0b726911185f3eab2cd0957dc5b2be893
Verner Fortelius <verner@fortelius.net>
2025-03-11 22:29:06 +0200

    move scripts around

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-npm run build export \
+cd build && npm run build export \
     ../adf/ \
     /subscriptions/ed5190ab-ab51-4f01-8a86-4825c1d7f284/resourceGroups/acme-adftest-rg/providers/Microsoft.DataFactory/factories/acmeish-dev-adf \
     "ExportedArmTemplate"
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,8 +4,8 @@ echo "Creating deployment stack"
 result=$(az stack group create \
   --name 'adftest-prod-adf' \
   --resource-group $rg \
-  --template-file '../build/ExportedArmTemplate/ARMTemplateForFactory.json' \
-  --parameters './parameters/parameters-prod.json' \
+  --template-file './build/ExportedArmTemplate/ARMTemplateForFactory.json' \
+  --parameters './deploy/parameters/parameters-prod.json' \
   --deny-settings-mode 'none' \
   --action-on-unmanage 'deleteResources' \
   --yes)
